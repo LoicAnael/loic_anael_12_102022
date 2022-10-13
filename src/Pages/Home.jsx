08fd15e35433f6a994911e8aae84e07d80236fb6
loic <loicanaelbdf@gmail.com>
2022-10-13 08:09:52 -0700

    file tree configuration

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +15,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
